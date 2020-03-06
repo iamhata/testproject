103acacd7023d26a28a191489e50744b9dedd225
neil chen <neil_chen@sercomm.com>
2020-03-06 13:49:46 +0800

    add fetch config function

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module test-module
 
 go 1.14
 
-require github.com/JustinSDK/pkgfoo v1.0.1
+require (
+	github.com/JustinSDK/pkgfoo v1.0.1
+	github.com/spf13/viper v1.6.2
+)
--- src/main/main.go
+++ src/main/main.go
@@ -6,6 +6,7 @@ import (
   "test-module/src/objects"
   "test-module/src/util"
   "time"
+  viper "github.com/spf13/viper"
 )
 
 func main() {
@@ -44,7 +45,21 @@ func main() {
   for _, i := range slice1 {
     fmt.Printf("res:%v \n", i)
   }
-  
+
+  viper.SetConfigName("application") // name of config file (without extension)
+  viper.SetConfigType("yaml") // REQUIRED if the config file does not have the extension in the name
+  viper.AddConfigPath("./config")   // path to look for the config file in
+  err := viper.ReadInConfig() // Find and read the config file
+  if err != nil { // Handle errors reading the config file
+    panic(fmt.Errorf("Fatal error config file: %s \n", err))
+  }
+  var map1 = viper.Get("coap")
+  var map2 = viper.GetStringMapString("coap")
+  v, exists := map2["namespace"]
+
+  fmt.Printf("read config:%v \n", map1)
+  fmt.Printf(" %v %t\n", v, exists)
+
   time.Sleep(5*time.Second)
 }
 
